a01e17248786bb5b6e3be7aed03b90a704cef6ff
heilokchow <u3514104@connect.hku.hk>
2020-09-01 21:55:08 +0800

    add interval determined parameters

--- R/paramteric.R
+++ R/paramteric.R
@@ -20,16 +20,19 @@
 #' on or before those specific time points.
 #'
 #' @param k number of orthogonal time dependent covariates
+#' @param t_start start time in range of (0,1)
+#' @param t_end end time in range of (0,1)
 #'
 #' @return
 #' \item{homo_coef}{List Individual's parameters' estimation}
 #' \item{homo_cov}{List Individual's parameters' covairance matrix}
 #' \item{nonhomo_coef}{List nonhomogeneous parameters' estimation}
 #' \item{nonhomo_cov}{List nonhomogeneous parameter's covariance estimation}
+#' \item{objective}{List of estimated parameters when t_start != 0 or t_end != 1}
 #'
 #' @export
 
-parametric <- function(data, zij, n, p, tz, k = 0) {
+parametric <- function(data, zij, n, p, tz, k = 0, t_start = 0, t_end = 1) {
 
   k1 = k
   dim_check = dim(zij)
@@ -54,7 +57,8 @@ parametric <- function(data, zij, n, p, tz, k = 0) {
 
   t_max <- (data[t, n1] - data[1, n1])
   t_norm <- (data[, n1] - data[1, n1][[1]]) / t_max[[1]]
-  t_sep_t = seq(0.01, 1, 0.01)
+  i_start <- which(t_norm >= t_start)[1]
+  i_end <- which(t_norm >= t_end)[1]
 
   hash = rep(0, n^2)
   co = 1
@@ -111,12 +115,15 @@ parametric <- function(data, zij, n, p, tz, k = 0) {
   N_t = matrix(0, nrow = n*(n-1), ncol = n2)
   b1k = matrix(0, nrow = (2*n-2), ncol = n2)
   sd1k = matrix(0, nrow = 2*n*n2, ncol = 2*n*n2)
+  V = matrix(0, nrow = n2*2*(n-1), ncol = n2*2*(n-1))
 
   b1 = matrix(0, nrow = 2, ncol = k1*2+1)
 
   temp0 = xConstruct(n, zij)
   temp = temp0$x
-  P1a = solve(t(temp) %*% temp) %*% t(temp)
+  Va = t(temp) %*% temp
+  P1a = solve(Va) %*% t(temp)
+  ba = matrix(NA, ncol = 1, nrow = 0)
 
   N_tz1 = matrix(0, nrow = n*(n-1), ncol = n2)
   N_tz2 = matrix(0, nrow = n*(n-1), ncol = n2^2)
@@ -128,22 +135,49 @@ parametric <- function(data, zij, n, p, tz, k = 0) {
   theta = matrix(0, nrow = p, ncol = n2)
   theta_cov = array(0, c(p*n2, p*n2))
 
-  t0 = 0
-  co_t = 1
+  t_0 = which(tz >= t_start)[1] - 1
+  t_1 = which(tz >= t_end)[1]
+  if (t_0 == 0) {
+    t0 = t_start
+  } else {
+    t0 = tz[t_0]
+  }
+  co_t = t_0 + 1
   t1 = tz[co_t]
 
+  err_flag = 0
+
   # Homogeneous Parameter Estimation
-  for (i in seq_len(t)) {
+  for (i in i_start:i_end) {
     z1 = data[i, 1]
     z2 = data[i, 2]
     z3 = hash[(z1-1)*n+z2]
 
     for (i1 in seq_len(n2)) {
       N_t[z3, i1] = N_t[z3, i1] + F_poly(k1, t_norm[i], i1)
-      if (t_norm[i] >= t_sep_t[100]) {
+      if (t_norm[i] >= t_end) {
         b1k[, i1] = P1a %*% matrix(N_t[, i1])
         b1[1, i1] = -sum(b1k[1:(n-1), i1])
         b1[2, i1] = -sum(b1k[n:(2*n-2), i1])
+        ba = rbind(ba, t(temp0$x) %*% matrix(N_t[, i1]))
+        for (j1 in i1:n2) {
+          F_poly_cov = function(x) {
+            if (i1 == 1 && j1 == 1) {
+              return(x^0)
+            } else {
+              return(F_poly(k1, x, i1) * F_poly(k1, x, j1))
+            }
+          }
+          idx1 = (i1-1)*(2*n-2)+1
+          idx2 = (j1-1)*(2*n-2)+1
+          if (i1 == j1) {
+            V[idx1:(idx1+2*n-3), idx2:(idx2+2*n-3)] = integrate(F_poly_cov, t_start, t_end)$value * Va
+          } else {
+            V[idx1:(idx1+2*n-3), idx2:(idx2+2*n-3)] = integrate(F_poly_cov, t_start, t_end)$value * Va
+            V[idx2:(idx2+2*n-3), idx1:(idx1+2*n-3)] = integrate(F_poly_cov, t_start, t_end)$value * Va
+          }
+        }
+
       }
     }
 
@@ -178,7 +212,7 @@ parametric <- function(data, zij, n, p, tz, k = 0) {
             count1[idx1:(idx1+p-1), idx2:(idx2+p-1)] = count1[idx1:(idx1+p-1), idx2:(idx2+p-1)] +
               temp1 * integrate(F_poly_cov, t0, t1)$value
             count3[idx1:(idx1+p-1), idx2:(idx2+p-1)] = count3[idx1:(idx1+p-1), idx2:(idx2+p-1)] +
-              t(temp) %*% diag(N_tz2[, idx]) %*% temp
+              t(temp) %*% diag(N_tz2[, idx0]) %*% temp
           } else {
             count1[idx1:(idx1+p-1), idx2:(idx2+p-1)] = count1[idx1:(idx1+p-1), idx2:(idx2+p-1)] +
               temp1 * integrate(F_poly_cov, t0, t1)$value
@@ -186,17 +220,19 @@ parametric <- function(data, zij, n, p, tz, k = 0) {
               temp1 * integrate(F_poly_cov, t0, t1)$value
 
             count3[idx1:(idx1+p-1), idx2:(idx2+p-1)] = count3[idx1:(idx1+p-1), idx2:(idx2+p-1)] +
-              t(temp) %*% diag(N_tz2[, idx]) %*% temp
+              t(temp) %*% diag(N_tz2[, idx0]) %*% temp
             count3[idx2:(idx2+p-1), idx1:(idx1+p-1)] = count3[idx2:(idx2+p-1), idx1:(idx1+p-1)] +
-              t(temp) %*% diag(N_tz2[, idx]) %*% temp
+              t(temp) %*% diag(N_tz2[, idx0]) %*% temp
           }
         }
       }
       count2[, ] = count2[, ] + count2_temp
       t0 = t1
-      if (co_t < length(tz)) {
-        co_t = co_t + 1
+      co_t = co_t + 1
+      if (co_t < t_1) {
         t1 = tz[co_t]
+      } else {
+        t1 = t_end
       }
       N_tz1 = matrix(0, nrow = n*(n-1), ncol = n2)
       N_tz2 = matrix(0, nrow = n*(n-1), ncol = n2^2)
@@ -218,26 +254,50 @@ parametric <- function(data, zij, n, p, tz, k = 0) {
     }
 
     # Non-Homogeneous Parameters' Estimation and standard deviation
-    if (t_norm[i] >= t_sep_t[100]) {
-      inv_count = solve(count1, count2)
-      for (z in seq_len(n2)) {
-        theta[, z] = inv_count[((z-1)*p+1):(z*p),1]
+    if (t_norm[i] >= t_end) {
+      inv_count = tryCatch(solve(count1, count2), error = function(e) NULL)
+      if (!is.null(inv_count)) {
+        for (z in seq_len(n2)) {
+          theta[, z] = inv_count[((z-1)*p+1):(z*p),1]
+        }
+        inv_count1 = solve(count1)
+        theta_cov = inv_count1 %*% count3 %*% inv_count1
+      } else {
+        err_flag = 1
       }
-      inv_count1 = solve(count1)
-      theta_cov = inv_count1 %*% count3 %*% inv_count1
     }
   }
 
   sd1k = sd1k / n^2
 
-  output <- list(homo_coef = list(outgoing = rbind(matrix(b1[1,], nrow = 1),
-                                                   as.matrix(b1k[1:(n-1),])),
-                                  incoming = rbind(matrix(b1[2,], nrow = 1),
-                                                   as.matrix(b1k[n:(2*n-2),]))),
-                 homo_cov = sd1k,
-                 nonhomo_coef = theta,
-                 nonhomo_cov = theta_cov,
-                 check = list(x = count1, y = count3))
+  # Construct Objective function Like Output
+
+  beta_homo = solve(V, ba)
+
+  if (t_start == 0 && t_end == 1) {
+    output <- list(homo_coef = list(outgoing = rbind(matrix(b1[1,], nrow = 1),
+                                                     as.matrix(b1k[1:(n-1),])),
+                                    incoming = rbind(matrix(b1[2,], nrow = 1),
+                                                     as.matrix(b1k[n:(2*n-2),]))),
+                   homo_cov = sd1k,
+                   nonhomo_coef = theta,
+                   nonhomo_cov = theta_cov,
+                   objective = list(beta_homo = beta_homo,
+                                    beta_nonh = inv_count,
+                                    V_homo = Va,
+                                    b_homo = ba,
+                                    V_nonh = count1,
+                                    b_nonh = count2,
+                                    check = err_flag))
+  } else {
+    output <- list(objective = list(beta_homo = beta_homo,
+                                    beta_nonh = inv_count,
+                                    V_homo = V,
+                                    b_homo = ba,
+                                    V_nonh = count1,
+                                    b_nonh = count2,
+                                    check = err_flag))
+  }
 
   output
 }
